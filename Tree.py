93331519cbe4f534f2289a01400c267590f5d409
lingxing <80627935+lingxinglovecode@users.noreply.github.com>
2021-04-14 15:48:19 +0800

    二叉树后序遍历

--- Tree.py
+++ Tree.py
@@ -115,50 +115,107 @@ class Solution:
                 cur1 = cur1.right
         return tree_list
 
+    #0-3 后序遍历
+    #后序遍历先遍历左子树，然后右子树，最后根节点
 
+    #方法1：递归
+    def postorderTraversal(self,root):
+        if root == None:
+            return []
+        return   self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
 
+    #方法2：栈
+    def postorderTravelsal(self,root):
+        if root == None:
+            return []
+        stack = list()
+        result = list()
 
+        node = root
+        prec = None
+        while node or stack:
+            while node != None:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            #当右侧没有节点或者当前节点已经被遍历过时输出值
+            if (not node.right) or (node.right == prec):
+                result.append(node.val)
+                prec = node
+                node = None
+            else:
+                stack.append(node)
+                node = node.right
+        return result
 
+    #方法3：Morris遍历
+    def postorderTravelsal(self, root):
 
+        def addPath(node):
+            count = 0
+            while node:
+                result.append(node.val)
+                node = node.right
+                count = count + 1
+            i = len(result)-count
+            j = len(result)-1
+            while i<j:
+                result[i],result[j] = result[j],result[i]
+                i += 1
+                j -= 1
 
+        if root == None:
+            return []
+        cur1 = root
+        result = list()
 
+        while cur1:
+            cur2 = cur1.left
+            if cur2:
+                while cur2.right and cur2.right != cur1:
+                    cur2 = cur2.right
+                if cur2.right == None:
+                    cur2.right = cur1
+                    cur1 = cur1.left
+                    continue
+                else:
+                    cur2.right = None
+                    addPath(cur1.left)
+            cur1 = cur1.right
+        addPath(root)
+        return result
 
+    #效率更低，因为每次都创建一个临时列表
+    def postorderTravelsal(self, root):
 
+        def save_list( node,result):
+            temp_list = list()
+            while node:
+                temp_list.append(node.val)
+                node = node.right
+            temp_list.reverse()
+            result = result + temp_list
+            return result
+        if root == None:
+            return []
+        cur1 = root
+        result = list()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        while cur1:
+            cur2 = cur1.left
+            if cur2:
+                while cur2.right and cur2.right != cur1:
+                    cur2 = cur2.right
+                if cur2.right == None:
+                    cur2.right = cur1
+                    cur1 = cur1.left
+                    continue
+                else:
+                    cur2.right = None
+                    result = save_list(cur1.left,result)
+            cur1 = cur1.right
+        result = save_list(root,result)
+        return result
 
 
 
@@ -173,11 +230,12 @@ class Solution:
 
 if __name__ == '__main__':
 
-    tree1_node2 = TreeNode(3)
-    tree1_node1 = TreeNode(2,left=tree1_node2)
-    tree1_root1 = TreeNode(1,right=tree1_node1)
+    tree1_node2 = TreeNode(1)
+    tree1_node1 = TreeNode(3,right=tree1_node2)
+    tree1_root1 = TreeNode(2,left=tree1_node1)
     solution = Solution()
     # list_tree_1 = solution.preorderTraversal(tree1_root1)
     # print(list_tree_1)
-    list_inorder = solution.inorderTraversal(tree1_root1)
-    print(list_inorder)
+    # list_inorder = solution.inorderTraversal(tree1_root1)
+    # print(list_inorder)
+    list_postorder = solution.postorderTravelsal(tree1_root1)
