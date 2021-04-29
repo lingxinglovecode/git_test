993650eabb1c59d2b6e66d09c0aec2376f0c1aa5
lingxinglovecode <80627935+lingxinglovecode@users.noreply.github.com>
2021-04-29 10:30:21 +0800

    2021.4.29

---
+++ 设计问题与数学/Math_problems.py
@@ -0,0 +1,68 @@
+import math
+
+class Math:
+
+    ##1.计数质数
+    #统计所有小于非负整数 n 的质数的数量。
+
+    def isPrimes(self,num):
+        if num == 0 or num == 1:
+            return False
+        if num == 2:
+            return True
+        for i in range(2, num):
+            if num % i != 0:
+                continue
+            return False
+        return True
+
+    def isPrimes(self,num):
+        if num == 0 or num == 1:
+            return False
+        if num == 2:
+            return True
+        if (num-1)%6!=0 and (num+1)%6!=0:
+            return False
+        for i in range(5, int(math.sqrt(num))+1,6):
+            if num % i == 0 or num % (i+2) == 0:
+                return False
+        return True
+    def countPrimes(self, n: int):
+
+        primes_dict = dict()
+        count = 0
+        for i in range(n):
+            if self.isPrimes(i) == True:
+                count = count+1
+        return count
+
+
+    def countPrimes(self,n):
+        if n<2:
+            return 0
+        elif n<=4:
+            return n-2
+        isPrimes = n*[1]
+        isPrimes[0] = 0
+        isPrimes[1] = 0
+        count = 0
+        for i in range(2,n):
+            if isPrimes[i] == 1:
+                count = count+1
+                for j in range(i*i,n,i):
+                    isPrimes[j] = 0
+        return count
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    math_problem = Math()
+    count = math_problem.countPrimes(10)
