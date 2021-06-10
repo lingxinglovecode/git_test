c509b92662bd72c485c50f2db38b0b82a5337e09
lingxinglovecode <80627935+lingxinglovecode@users.noreply.github.com>
2021-06-10 09:03:58 +0800

    用队列实现队栈

--- 数据结构与类型/Stack.py
+++ 数据结构与类型/Stack.py
@@ -6,6 +6,100 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+
+#用队列实现栈
+
+#方法1：用一个队列暂存元素
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+        self.temp_queue = collections.deque()
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        length = len(self.queue)
+        while length-1:
+            self.temp_queue.append(self.queue.popleft())
+            length = length-1
+        res = self.queue.popleft()
+        self.queue = self.temp_queue
+        return res
+
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        length = len(self.queue)
+        while length - 1:
+            self.temp_queue.append(self.queue.popleft())
+            length = length - 1
+        res = self.queue[0]
+        self.temp_queue.append(self.queue.popleft())
+        self.queue = self.temp_queue
+        return res
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.queue) == 0
+
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque()
+
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+        for i in range(0,len(self.queue)-1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.queue.popleft()
+
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.queue[0]
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        return len(self.queue) == 0
+
+
+
+
 class Solution:
 
 
@@ -232,3 +326,13 @@ if __name__ == '__main__':
     target = 3
     ways = solution.findTargetSumWays(nums,target)
     print(ways)
+
+    # Your MyStack object will be instantiated and called as such:
+    obj = MyStack()
+    x = 7
+    y=9
+    obj.push(x)
+    obj.push(y)
+    param_2 = obj.pop()
+    param_3 = obj.top()
+    param_4 = obj.empty()
